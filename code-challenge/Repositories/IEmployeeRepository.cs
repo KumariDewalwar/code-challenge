cc20c8352913dce1e61930b76e4f90ea1d5cc580
RajaKumari Dewalwar <rdewalwar@paychex.com>
2024-08-26 21:55:17 +0530

    Add files via upload

---
+++ code-challenge/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using challenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace challenge.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetById(String id);
+        Employee Add(Employee employee);
+        Task SaveAsync();
+
+        Employee Remove(Employee employee);
+
+        Task<Employee> UpdatedirectReports(string employeeId, List<Employee> newdirectReports);
+
+
+    }
+}
